ca8ac9feabbcae56b42fdf878a44d200233614b3
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-30 09:13:38 -0600

    Feat: add is hoarded to product

--- lib/product.rb
+++ lib/product.rb
@@ -7,10 +7,19 @@ class Product
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @hoard = false
   end
 
   def total_price
     return @unit_price * @quantity
+  end
+
+  def is_hoarded?
+    return @hoard
+  end
+
+  def hoard
+    @hoard = true
   end
 
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -40,5 +40,13 @@ describe Product do
       expect(product1.total_price).to eq 37.0
       expect(product2.total_price).to eq 9.0
     end
+
+    it 'Product can determine if its hoarded' do
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+      expect(product1.is_hoarded?).to eq false
+      product1.hoard
+      expect(product1.is_hoarded?).to eq true
+    end
   end
 end
