c413b22715cc84b65c00929dde2f85462f87a3da
Bryce Wein <bryce.wein@gmail.com>
2022-03-30 11:11:53 -0400

    Refactor: Remove attrr for hoarded.

--- lib/product.rb
+++ lib/product.rb
@@ -1,5 +1,5 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity, :hoarded
+  attr_reader :category, :name, :unit_price, :quantity
 
   def initialize(category, name, unit_price, quantity)
     @category = category
