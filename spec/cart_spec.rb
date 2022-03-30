f4d25dac66f7b1228a41e26b098026e49faec701
Shirley DeCesari <shirley.decesari@gmail.com>
2022-03-30 10:00:13 -0700

    Create test two for add product

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -7,10 +7,19 @@ describe Cart do
   context 'Iteration 2' do
     it 'Product exists' do
       cart = Cart.new("King Soopers", "30items")
-      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-      product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
       expect(cart).to be_an_instance_of(Cart)
     end
+  end
+
+  it 'Add product' do
+    cart = Cart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    expect(cart.products).to eq([product1, product2])
   end
 end
