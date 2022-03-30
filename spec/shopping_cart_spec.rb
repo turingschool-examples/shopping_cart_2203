886e145ee33bbac391150a27b34dc1d4768da94c
Bryce Wein <bryce.wein@gmail.com>
2022-03-30 11:48:39 -0400

    Test: Add test for sorted products

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -76,4 +76,16 @@ describe ShoppingCart do
     cart.add_product(product3)
     expect(cart.percentage_occupied).to eq(43.33)
   end
+  it 'can produce sorted array of products by quantity, highest to lowest' do
+    cart = ShoppingCart.new('King Soopers', '30items')
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product4)
+    expect(cart.sorted_products_by_quantity).to eq([product4, product1, product2, product3])
+  end
 end
