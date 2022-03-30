c70484b5d6659fea6b4c2da4179498b4c008b146
Lucas Colwell <colwellc37@gmail.com>
2022-03-30 10:00:04 -0600

    Feat: add test for shopping cart class

---
+++ spec/shopping_cart_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/product'
+require './lib/shopping_cart'
+require 'pry'
+
+describe ShoppingCart do
+  it "exists" do
+    cart = ShoppingCart.new("King Soopers", "30 items")
+    expect(cart).to be_an_instance_of(ShoppingCart)
+  end
+
+  it "can read attributes" do
+    cart = ShoppingCart.new("King Soopers", "30 items")
+    expect(cart.name).to eq "King Soopers"
+    expect(cart.capacity).to eq 30
+  end
+
+
+
+
+end
