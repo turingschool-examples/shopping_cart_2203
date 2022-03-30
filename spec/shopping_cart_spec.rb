1d7b2431465438e25666f112c6d279397cbab3c0
Bryce Wein <bryce.wein@gmail.com>
2022-03-30 11:33:06 -0400

    Test: Add is full test

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -41,4 +41,14 @@ describe ShoppingCart do
     cart.add_product(product3)
     expect(cart.total_number_of_products).to eq(13)
   end
+  it 'can tell if it is full or not' do
+    cart = ShoppingCart.new('King Soopers', '30items')
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    expect(cart.is_full?).to eq(false)
+  end
 end
