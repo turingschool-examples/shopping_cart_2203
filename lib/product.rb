6a706c075351e3a57cb19bc508199b00bdfc98e8
Colin Reinhart <colinbreinhartgmail.com>
2022-03-30 11:02:52 -0600

    Initial Commit

---
+++ lib/product.rb
@@ -0,0 +1,20 @@
+class Product
+  attr_reader :category, :name, :unit_price, :quantity
+
+  def initialize(category, name, unit_price, quantity)
+    @category = category
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity
+    @is_hoarded = false
+  end
+
+  def total_price
+    @unit_price * @quantity.to_f
+  end
+
+  def hoard
+    @is_hoarded = true
+  end
+
+end
