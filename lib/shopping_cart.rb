8ea413e577538ff962ddf9bc6b51ed94385f7c9d
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-30 12:56:21 -0400

    Add most of Iteration 3

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,6 +1,7 @@
 
 class ShoppingCart
   attr_reader :name, :capacity, :products
+
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
@@ -17,4 +18,19 @@ class ShoppingCart
       capacity: @capacity.chomp("items").to_i,
     }
   end
+
+  def total_number_of_products
+    quantities = []
+    @products.each do |product|
+    quantities << product.quantity
+    end
+    total = quantities.reduce(0) do |sum, number|
+      sum + number
+    end
+  end
+  #
+  # def is_full?
+  #   if total > @capacity
+  # end
+  #
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -48,5 +48,18 @@ describe ShoppingCart do
       # require "pry"; binding.pry
       expect(cart.details).to eq({name: "King Soopers", capacity: 30})
     end
+
+    it 'ShoppingCart can return total number of products' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+ # require "pry"; binding.pry
+      expect(cart.total_number_of_products).to eq(13)
+      # expect(cart.is_full?).to eq(false)
+    end
   end
 end
