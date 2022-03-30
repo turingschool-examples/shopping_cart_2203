77ad0bc22153702d4987db5b477234b1ef2a7128
Sarah Hall <swatson1994@yahoo.com>
2022-03-30 11:54:54 -0400

    "Feat: Add readable details hash with name and capacity"

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,11 +1,15 @@
 require './lib/product'
 
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :details
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.to_i
     @products = []
+    @details = {
+      name: @name,
+      capacity: @capacity
+    }
   end
 
   def add_product(product)
