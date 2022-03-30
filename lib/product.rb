6333fec6fcff02e0069ab11f66dfd3e044442006
Matt Deming <vballmatt44@gmail.com>
2022-03-30 09:17:29 -0600

    test: pass second test

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,14 @@
 require 'pry'
 
 class Product
-  attr_reader
+  attr_reader :category, :name
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
-    
+
   end
 
+  # def category
+  #   product.category << category
+  # end
 end
