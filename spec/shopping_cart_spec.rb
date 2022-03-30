5a1169f7d4e62630285c92ef9f33d917037344dc
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-30 09:26:58 -0600

    Feat: add add product to shopping cart

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -6,17 +6,27 @@ require './lib/shopping_cart'
 describe ShoppingCart do
   context 'iteration 2' do
     it 'Shopping cart exists' do
-      cart1 = ShoppingCart.new("King Soopers", "30items")
+      cart = ShoppingCart.new("King Soopers", "30items")
 
-      expect(cart1).to be_an_instance_of ShoppingCart
+      expect(cart).to be_an_instance_of ShoppingCart
     end
 
-    it 'has readable attributes' do
-      cart1 = ShoppingCart.new("King Soopers", "30items")
+    it 'Shopping Cart has readable attributes' do
+      cart = ShoppingCart.new("King Soopers", "30items")
 
-      expect(cart1.name).to eq "King Soopers"
-      expect(cart1.capacity).to eq 30
-      expect(cart1.products).to eq []
+      expect(cart.name).to eq "King Soopers"
+      expect(cart.capacity).to eq 30
+      expect(cart.products).to eq []
+    end
+
+    it 'Shopping Cart can add products to its products array' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      cart.add_product(product1)
+      cart.add_product(product2)
+
+      expect(cart.products).to eq [product1, product2]
     end
   end
 end
