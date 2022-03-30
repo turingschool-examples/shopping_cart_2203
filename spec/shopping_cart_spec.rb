1b6a28336ebd32e5d872fcda87b32221c52cf1c1
Bryce Wein <bryce.wein@gmail.com>
2022-03-30 11:16:54 -0400

    Test: Add attributes test

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -7,4 +7,11 @@ describe ShoppingCart do
     cart = ShoppingCart.new('King Soopers', '30items')
     expect(cart).to be_a(ShoppingCart)
   end
+
+  it 'has attributes' do
+    cart = ShoppingCart.new('King Soopers', '30items')
+    expect(cart.name).to eq('King Soopers')
+    expect(cart.capacity).to eq(30)
+    expect(cart.products).to eq([])
+  end
 end
