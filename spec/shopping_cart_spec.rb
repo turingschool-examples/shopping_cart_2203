28cd5f7656819e2f03e43eeb5272a3d055141233
Stephen Wilkens <stephenwilkens@gmail.com>
2022-03-30 10:59:11 -0600

    Update shopping cart class and tests for iteration 3

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -6,6 +6,8 @@ describe ShoppingCart do
   let(:shopping_cart){ShoppingCart.new("King Soopers", "30items")}
   let(:product1){Product.new(:paper, 'toilet paper', 3.70, '10')}
   let(:product2){Product.new(:meat, 'chicken', 4.50, '2')}
+  let(:product3){Product.new(:paper, 'tissue paper', 1.25, '1')}
+  let(:product4){Product.new(:produce, 'apples', 0.99, '20')}
 
   it "exists" do
     expect(shopping_cart).to be_an_instance_of(ShoppingCart)
@@ -26,6 +28,22 @@ describe ShoppingCart do
 
   it 'can read cart details' do
     expect(shopping_cart.details).to eq([shopping_cart.name, shopping_cart.capacity])
-    # binding.pry
+  end
+
+  it 'is full?' do
+    shopping_cart.add_product(product1)
+    shopping_cart.add_product(product2)
+    shopping_cart.add_product(product3)
+    expect(shopping_cart.is_full?).to eq(false)
+    shopping_cart.add_product(product4)
+    expect(shopping_cart.is_full?).to eq(true)
+  end
+
+  it 'can return an array of products by category' do
+    shopping_cart.add_product(product1)
+    shopping_cart.add_product(product2)
+    shopping_cart.add_product(product3)
+    shopping_cart.add_product(product4)
+    expect(shopping_cart.products_by_category(:paper)).to eq([product1, product3])
   end
 end
