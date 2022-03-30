3b04bda09252a75e2b5203b1aa8dfb83e31c631c
Sarah Hall <swatson1994@yahoo.com>
2022-03-30 11:23:24 -0400

    Feat: add is_hoarded method

--- lib/product.rb
+++ lib/product.rb
@@ -5,8 +5,15 @@ class Product
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @being_hoarded = false
   end
+
   def total_price
     (@quantity.to_f) * @unit_price
   end
+
+  def is_hoarded?
+    @being_hoarded
+  end
+  
 end
