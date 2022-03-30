854ba32aeeec8bd924eb1127b0baf2f9b2c596ed
Bryce Wein <bryce.wein@gmail.com>
2022-03-30 11:54:33 -0400

    Test: Create product breakdown test

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -88,4 +88,16 @@ describe ShoppingCart do
     cart.add_product(product4)
     expect(cart.sorted_products_by_quantity).to eq([product4, product1, product2, product3])
   end
+  it 'can produce a product breakdown' do
+    cart = ShoppingCart.new('King Soopers', '30items')
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product4)
+    expect(cart.product_breakdown).to eq({ meat: [product2], paper: [product1, product3], produce: [product4] })
+  end
 end
