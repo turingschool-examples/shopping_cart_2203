b56d123a43f102cff3da0d3c701bae371877c8f1
Lucas Colwell <colwellc37@gmail.com>
2022-03-30 09:24:20 -0600

    Feat: added total price method

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,18 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity
-  def initialize(category, name, unit_price, quantity)
+  attr_reader :category, :name, :unit_price, :quantity, :total_price
+  def initialize(category, name, unit_price, quantity) #, total_price)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity
     #is_hoarded = []
   end
+
+  def total_price
+    total = @quantity.to_f * @unit_price
+  end
+
+
+
+
 end
