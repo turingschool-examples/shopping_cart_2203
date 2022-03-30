699a495fe398f635b368bce987db39354a639084
Wesley Garcia <wes@alltradestech.com>
2022-03-30 09:16:10 -0700

    Finish iteration 3; test: add tests for it4

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -18,4 +18,30 @@ class ShoppingCart
       capacity: @capacity
     }
   end
+
+  def total_number_of_products
+    total_products = 0
+    @products.each do |product|
+      total_products += product.quantity
+    end
+    total_products
+  end
+
+  def is_full?
+    total_products = 0
+    @products.each do |product|
+      total_products += product.quantity
+    end
+    total_products > @capacity
+  end
+
+  def products_by_category(type)
+    product_by_cat = []
+    @products.each do |product|
+      if product.category == type
+        product_by_cat << product
+      end
+    end
+    product_by_cat
+  end
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -37,4 +37,84 @@ RSpec.describe ShoppingCart do
     cart = ShoppingCart.new("King Soopers", "30items")
     expect(cart.details).to eq({name: "King Soopers", capacity: 30})
   end
+
+  it "counts products" do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, "toilet paper", 3.70, "10")
+    product2 = Product.new(:meat, "chicken", 4.50, "2")
+    product3 = Product.new(:paper, "tissue paper", 1.25, "1")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    expect(cart.total_number_of_products).to eq 13
+  end
+
+  it "can be full" do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, "toilet paper", 3.70, "10")
+    product2 = Product.new(:meat, "chicken", 4.50, "2")
+    product3 = Product.new(:paper, "tissue paper", 1.25, "1")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    expect(cart.is_full?).to eq false
+    product4 = Product.new(:produce, "apples", 0.99, "20")
+    cart.add_product(product4)
+    expect(cart.is_full?).to eq true
+  end
+
+  it "can show products by category" do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, "toilet paper", 3.70, "10")
+    product2 = Product.new(:meat, "chicken", 4.50, "2")
+    product3 = Product.new(:paper, "tissue paper", 1.25, "1")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, "apples", 0.99, "20")
+    cart.add_product(product4)
+    expect(cart.products_by_category(:paper)).to eq([product1, product3])
+  end
+
+  xit "can determine capacity percentage" do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, "toilet paper", 3.70, "10")
+    product2 = Product.new(:meat, "chicken", 4.50, "2")
+    product3 = Product.new(:paper, "tissue paper", 1.25, "1")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    expect(cart.percentage_occupied).to eq 43.33
+  end
+
+  xit "can sort products by quantity" do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, "toilet paper", 3.70, "10")
+    product2 = Product.new(:meat, "chicken", 4.50, "2")
+    product3 = Product.new(:paper, "tissue paper", 1.25, "1")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, "apples", 0.99, "20")
+    cart.add_product(product4)
+    expect(cart.sorted_products_by_quantity).to eq(
+      [product4, product1, product2, product3]
+    )
+  end
+
+  xit "can group products by category" do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, "toilet paper", 3.70, "10")
+    product2 = Product.new(:meat, "chicken", 4.50, "2")
+    product3 = Product.new(:paper, "tissue paper", 1.25, "1")
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, "apples", 0.99, "20")
+    cart.add_product(product4)
+    expect(cart.product_breakdown).to eq(
+      {meat: [product2], paper: [product1, product3],
+       produce: [product4]}
+    )
+  end
 end
