57276669ef222552d2fa48a54abc0bc8d245e77e
Stirling Hostetter <stirhoss@icloud.com>
2022-03-30 11:42:25 -0500

    Complete Iteration 3

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,45 @@
+require 'pry'
+require './lib/product'
+
+class ShoppingCart
+  attr_reader :name,
+              :capacity,
+              :products
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity.to_i
+    @products = []
+   end
+
+   def add_product(product)
+     @products << product
+   end
+
+   def details
+     @details = {
+       :name => @name,
+       :capacity => @capacity
+     }
+   end
+
+   def total_number_of_products
+     total = 0
+     products.each do |e|
+       total += e.quantity
+     end
+     total
+   end
+
+   def is_full?
+     total_number_of_products >= capacity
+   end
+
+   def products_by_category(category)
+     this_cat = []
+     products.each do |e|
+       this_cat << e if e.category == category
+     end
+     this_cat
+   end
+end
---
+++ spec/shopping_cart_spec.rb
@@ -0,0 +1,106 @@
+require './lib/product'
+require './lib/shopping_cart'
+
+
+describe ShoppingCart do
+  it 'creates a new cart and takes 2 arguments' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    expect(cart).to be_an_instance_of ShoppingCart
+  end
+
+  it 'can report the name of the cart' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    expect(cart.name).to eq "King Soopers"
+  end
+
+  it '#capacity can take in a string and convert it to an integer' do
+  cart = ShoppingCart.new("King Soopers", "30items")
+
+  expect(cart.capacity).to eq 30
+  end
+
+  it '#products can return the array @products' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    expect(cart.products).to eq []
+  end
+
+  it '#add_product to cart' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper',3.70, '10')
+    product2 = Product.new(:meat, 'chicken',4.50, '2')
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    expect(cart.products).to eq [product1, product2]
+  end
+
+  xit '#details returns a hash of cart details' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper',3.70, '10')
+    product2 = Product.new(:meat, 'chicken',4.50, '2')
+    cart.add_product(product1)
+    cart.add_product(product2)
+
+    #unsure how to format this test/what the interaction pattern is looking for
+    expect(cart.details).to eq(@details)
+  end
+
+  it '#total_number_of_products returns the number of products in cart' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper',3.70, '10')
+    product2 = Product.new(:meat, 'chicken',4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    expect(cart.total_number_of_products).to eq 13
+  end
+
+  it '#is_full? returns boolean to check if cart is at capacity' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper',3.70, '10')
+    product2 = Product.new(:meat, 'chicken',4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    expect(cart.is_full?).to eq false
+  end
+
+  it '#is_full returns true when at or over capacity' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper',3.70, '10')
+    product2 = Product.new(:meat, 'chicken',4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, 'apples', 0.99,'20')
+    cart.add_product(product4)
+
+    expect(cart.is_full?).to eq true
+  end
+
+  it '#products_by_category shows you all products of 1 category' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper',3.70, '10')
+    product2 = Product.new(:meat, 'chicken',4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, 'apples', 0.99,'20')
+    cart.add_product(product4)
+
+    expect(cart.products_by_category(:paper)).to eq [product1, product3]
+    expect(cart.products_by_category(:meat)).to eq [product2]
+    expect(cart.products_by_category(:produce)).to eq [product4]
+  end
+
+  
+end
