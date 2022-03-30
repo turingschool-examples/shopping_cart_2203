6a6b6e1509fb9dfe11309e9bf800c036f86c6515
Stirling Hostetter <stirhoss@icloud.com>
2022-03-30 11:55:03 -0500

    Test #sorted_products_by_quantity written, no time to complete

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -113,4 +113,18 @@ describe ShoppingCart do
 
     expect(cart.percentage_occupied).to eq 43.33
   end
+
+  it '#sorted_products_by_quantity sorts products by quantity in cart' do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper',3.70, '10')
+    product2 = Product.new(:meat, 'chicken',4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    product4 = Product.new(:produce, 'apples', 0.99,'20')
+    cart.add_product(product4)
+
+    expect(cart.sorted_products_by_quantity).to eq[product3, product2, product1, product4]
+  end
 end
