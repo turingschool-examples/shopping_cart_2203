2d4d4d33817119d4756469a6178d13c197af9454
Sarah Hall <swatson1994@yahoo.com>
2022-03-30 11:48:45 -0400

    "Test: Add test for shopping cart to add and store products"

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -27,5 +27,16 @@ describe ShoppingCart do
 
       expect(cart.products).to eq([])
     end
+
+    it 'ShoppingCart can have products added to it' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+
+      expect(cart.products).to eq([product1, product2])
+    end
   end
 end
