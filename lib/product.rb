ac41f749e78d93b25a807631aa536a07b2fa31cd
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-30 11:40:11 -0400

    Initial commit: passes all tests for iteration 1

---
+++ lib/product.rb
@@ -0,0 +1,22 @@
+class Product
+attr_reader :category, :name, :unit_price, :quantity
+
+  def initialize(category, name, unit_price, quantity)
+    @category = category
+    @name = name
+    @unit_price = unit_price
+    @quantity = quantity
+  end
+
+  def total_price
+    price = @unit_price * @quantity.to_f
+    p price
+  end
+end
+
+# product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+# product2 = Product.new(:meat, 'chicken', 4.50, '2')
+#
+#
+# product1.total_price
+# require 'pry' ; binding.pry
