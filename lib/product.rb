82331237cf48002e65488d4a4a36a366aae86931
Lucas Colwell <colwellc37@gmail.com>
2022-03-30 09:42:16 -0600

    Feat: add hoard method and boolean

--- lib/product.rb
+++ lib/product.rb
@@ -1,18 +1,29 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity, :total_price
+  attr_reader :category, :name, :unit_price, :quantity
   def initialize(category, name, unit_price, quantity) #, total_price)
     @category = category
     @name = name
     @unit_price = unit_price
-    @quantity = quantity
+    @quantity = quantity.to_f
     #is_hoarded = []
   end
 
   def total_price
-    total = @quantity.to_f * @unit_price
+    @quantity * @unit_price
   end
 
+  def hoard
+    @quantity += 20
+  end
 
+  def is_hoarded?
+    if @quantity < 11
+      false
+    else
+      true
+    end
+
+  end
 
 
 end
